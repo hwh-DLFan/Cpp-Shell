f766acb35ef5a1572cdf3820dc056dad7764a092
hwh-DLFan <152786945+hwh-DLFan@users.noreply.github.com>
2024-02-04 10:34:09 +0800

    Start up files
    These files are the basic files.
    
    Signed-off-by: hwh-DLFan <152786945+hwh-DLFan@users.noreply.github.com>

---
+++ C++ Shell/DOS/DOS.cpp
@@ -0,0 +1,23 @@
+﻿// DOS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdlib>
+bool powershell = true;
+int main()
+{
+    if (std::system("type %windir%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe") && std::system("type %ProgramFiles%\\PowerShell\\7\\pwsh.exe") && std::system("type %ProgramFiles%\\PowerShell\\6\\pwsh.exe") && std::system("type %ProgramFiles(x86)\\PowerShell\\7\\pwsh.exe") && std::system("type %ProgramFiles%\\PowerShell\\6\\pwsh.exe")) powershell = false;
+    std::system("cls");
+    std::cout << "Thank you for using this application!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
